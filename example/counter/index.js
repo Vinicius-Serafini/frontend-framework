30b2b672308b8927697cdc1e8ad0d7c053159306
Vinicius Serafini <viniciusc.serafini@gmail.com>
2024-09-22 23:57:48 -0300

    exemplo

---
+++ example/counter/index.js
@@ -0,0 +1,41 @@
+import { computed, elements, observableOf } from "../../framework/index.js";
+const { h1, div, p, button } = elements;
+
+const root = document.getElementById('app');
+
+function App() {
+  const count = observableOf(0);
+
+  const counterClass = computed(() => {
+    if (count.get() < 0) {
+      return 'counter--danger';
+    }
+
+    if (count.get() > 0) {
+      return 'counter--success';
+    }
+
+    return 'counter--neutral';
+  });
+
+  return div({
+    class: 'counter'
+  },
+    h1({ class: 'counter__title' }, "Counter"),
+    p({
+      class: ['counter__count', counterClass],
+    }, 'Count: ', count),
+    div({ class: 'counter__buttons' }, [
+      button({
+        onclick: () => count.update((x) => x + 1),
+        class: 'counter__button counter__button--success'
+      }, '👆'),
+      button({
+        onclick: () => count.update((x) => x - 1),
+        class: 'counter__button counter__button--danger'
+      }, '👇'),
+    ])
+  );
+}
+
+root.append(App());
